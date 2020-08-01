9029bdbf8f2794d227e3c4ae6b7cb945882e2f87
lxy878 <lxy878@msn.cn>
2020-08-01 03:34:42 -0400

    cleaned code and added test seed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+    def email(user)
+        user.email ? user.email : 'None'
+    end
+
+    def phone_number(user)
+        user.phone_number ? user.phone_number : 'None'
+    end
 end
